683c2754a7df9b74e5b6f39afb6d4f6728acb747
cnryan <cnhongbinli@gmail.com>
2021-09-23 23:20:16 +0800

    添加冴羽版的节流与防抖.

--- src/function/debounce.js
+++ src/function/debounce.js
@@ -20,3 +20,52 @@ export function debounce(callback, wait) {
     }, wait)
   }
 }
+
+/**
+ * 冴羽版
+ * 防抖的原理就是：你尽管触发事件，但是我一定在事件触发 n 秒后才执行
+ * 如果你在一个事件触发的 n 秒内又触发了这个事件，那我就以新的事件的时间为准，n 秒后才执行
+ * 总之，就是要等你触发完事件 n 秒内不再触发事件，我才执行
+ */
+
+/**
+ * 需要考虑的问题
+ * 1. this的指向
+ * 2. event对象
+ * 3. 立即执行: 立刻执行函数，然后等到停止触发 n 秒后，才可以重新触发执行
+ * 4. 返回值
+ * 5. 取消
+ */
+export function debounceAdvanced(func, wait, immediate) {
+  let timeout, result
+  const debounced = function() {
+    let context = this
+    let args = arguments
+
+    if (timeout) {
+      clearTimeout(timeout)
+    }
+
+    if (immediate) {
+      let callNow = !timeout
+      timeout = setTimeout(function() {
+        timeout = null
+      }, wait)
+      if (callNow) {
+        result = func.apply(context, args)
+      }
+    } else {
+      timeout = setTimeout(function() {
+        func.apply(context, args)
+      }, wait)
+    }
+    return result
+  }
+
+  debounced.cancel = function() {
+    clearTimeout(timeout)
+    timeout = null
+  }
+
+  return debounced
+}
--- src/function/throttle.js
+++ src/function/throttle.js
@@ -24,5 +24,117 @@ export function throttle(callback, wait) {
   }
 }
 
+/**
+ * 冴羽版: 如果你持续触发事件，每隔一段时间，只执行一次事件
+ * 根据首次是否执行以及结束后是否执行，效果有所不同，实现的方式也有所不同
+ * 我们用 leading 代表首次是否执行，trailing 代表结束后是否再执行一次
+ */
 
+// 方式1：使用时间戳
+// 立刻执行, 停止触发后没有办法再执行事件
+function throttleAdvanced1(func, wait) {
+  let context, args
+  let previous = 0
 
+  return function() {
+    let now = +new Date()
+    context = this
+    args = arguments
+    if (now - previous > wait) {
+      func.apply(context, args)
+      previous = now
+    }
+  }
+}
+// 方式2：使用定时器
+// 在 n 秒后第一次执行, 停止触发后依然会再执行一次事件
+function throttleAdvanced2(func, wait) {
+  let timeout
+  let context, args
+
+  return function() {
+    context = this
+    args = arguments
+    if (!timeout) {
+      timeout = setTimeout(function() {
+        timeout = null
+        func.apply(context, args)
+      }, wait)
+    }
+  }
+}
+
+// 方式1和2结合
+// 头部立即执行一次，尾部停止触发后还会再执行一次
+function throttleAdvanced(func, wait) {
+  let timeout, context, args, result
+  let previous = 0
+
+  const later = function() {
+    previous = +new Date()
+    timeout = null
+    func.apply(context, args)
+  }
+
+  return function() {
+    let now = +new Date()
+    // 下次触发 func 剩余的时间
+    let remaining = wait - (now - previous)
+    context = this
+    args = arguments
+    // 如果没有剩余的时间了或者你改了系统时间
+    if (remaining <= 0 || remaining > wait) {
+      if (timeout) {
+        clearTimeout(timeout)
+        timeout = null
+      }
+      previous = now
+      func.apply(context, args)
+    } else if (!timeout) {
+      timeout = setTimeout(later, remaining)
+    }
+  }
+}
+
+// options.leading：false 和 options.trailing: false 不能同时设置
+function throttleAdvancedWithOption(func, wait, options) {
+  let timeout, context, args, result
+  let previous = 0
+  options = options || {}
+
+  const later = function() {
+    previous = options.leading === false ? 0 : +new Date()
+    timeout = null
+    func.apply(context, args)
+    context = args = null // 垃圾回收
+  }
+
+  const throttled =  function() {
+    let now = +new Date()
+    if (!previous && options.leading === false) previous = now
+    // 下次触发 func 剩余的时间
+    let remaining = wait - (now - previous)
+    context = this
+    args = arguments
+    // 如果没有剩余的时间了或者你改了系统时间
+    if (remaining <= 0 || remaining > wait) {
+      if (timeout) {
+        clearTimeout(timeout)
+        timeout = null
+      }
+      previous = now
+      func.apply(context, args)
+      if (!timeout) context = args = null
+    } else if (!timeout && options.trailing !== false) {
+      timeout = setTimeout(later, remaining)
+    }
+  }
+
+  throttled.cancel = function() {
+    clearTimeout(timeout)
+    previous = 0
+    timeout = null
+  }
+
+  return throttled
+}
