5a84adad249038b52e591561e54dcd8fb84bc985
cnryan <cnhongbinli@gmail.com>
2021-09-23 23:45:21 +0800

    优化数组去重.

--- src/array/unique.js
+++ src/array/unique.js
@@ -17,20 +17,36 @@ export function unique1(array) {
 
 // 方法2: 利用forEach() + 对象容器
 // 只需一重遍历, 效率高些
+// 需要考虑 item 可能是1和"1"的问题，也要考虑item可能是个对象
 export function unique2(array) {
-  const arr = []
+  // const arr = []
+  // const obj = {}
+  // array.forEach(item => {
+  //   if (!obj.hasOwnProperty(typeof item + JSON.stringify(item))) {
+  //     obj[typeof item + JSON.stringify(item)] = true
+  //     arr.push(item)
+  //   }
+  // })
+  // return arr
+
   const obj = {}
-  array.forEach(item => {
-    if (!obj.hasOwnProperty(item)) {
-      obj[item] = true
-      arr.push(item)
-    }
+  return array.filter((item, index, array) => {
+    return obj.hasOwnProperty(typeof item + JSON.stringify(item)) ?
+      false :
+      (obj[typeof item + JSON.stringify(item)] = true)
   })
-  return arr
 }
 
 // 方法3：利用ES6语法from+Set, ...+Set
 // 编码简洁
 export function unique3(array) {
   return [...new Set(array)]
+}
+
+
+// 方法4：利用 filter 方法
+export function unique4(array) {
+  return array.filter((item, index, array) => {
+    return array.indexOf(item) === index
+  })
 }
--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ export { debounce } from './function/debounce'
 
 /** 数组工具 **/
 export { map, reduce, filter, find, findIndex, every, some} from './array/declares'
-export { unique1, unique2, unique3 } from './array/unique'
+export { unique1, unique2, unique3, unique4 } from './array/unique'
 export { concat } from './array/concat'
 export { slice } from './array/slice'
 export { flatten1, flatten2 } from './array/flatten'
