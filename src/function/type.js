0f2363b43c5326cde7a84224ac315026b46b8701
cnryan <cnhongbinli@gmail.com>
2021-09-25 11:53:45 +0800

    类型判断.

---
+++ src/function/type.js
@@ -0,0 +1,83 @@
+const class2type = {}
+
+'Boolean Number String Function Array Date RegExp Object Error Null Undefined'.split(' ').map((item, index) => {
+  class2type['[object ' + item + ']'] = item.toLowerCase()
+})
+
+function type(obj) {
+  return typeof obj === 'object' || typeof obj === 'function' ?
+    class2type[Object.prototype.toString.call(obj)] || 'object' : typeof obj
+}
+
+export const isFunction = function(obj) {
+  return type(obj) === 'function'
+}
+
+export const isArray = Array.isArray || function(obj) {
+  return type(obj) === 'array'
+}
+
+export const isPlainObject = function(obj) {
+  let proto, Ctor
+
+  // 排除掉明显不是obj的以及一些宿主对象如Window
+  if (!obj || Object.prototype.toString.call(obj) !== "[object Object]") {
+    return false
+  }
+
+  /**
+   * getPrototypeOf es5 方法，获取 obj 的原型
+   * 以 new Object 创建的对象为例的话
+   * obj.__proto__ === Object.prototype
+   */
+  proto = Object.getPrototypeOf(obj)
+
+  // 没有原型的对象是纯粹的，Object.create(null) 就在这里返回 true
+  if (!proto) {
+    return true
+  }
+
+  /**
+   * 以下判断通过 new Object 方式创建的对象
+   * 判断 proto 是否有 constructor 属性，如果有就让 Ctor 的值为 proto.constructor
+   * 如果是 Object 函数创建的对象，Ctor 在这里就等于 Object 构造函数
+   */
+  Ctor = Object.prototype.hasOwnProperty.call(proto, "constructor") && proto.constructor
+
+  // 在这里判断 Ctor 构造函数是不是 Object 构造函数，用于区分自定义构造函数和 Object 构造函数
+  // 这里用了函数的 toString方法。
+  // Function 对象覆盖了从 Object 继承来的 Object.prototype.toString 方法。函数的 toString 方法会返回一个表示函数源代码的字符串。
+  // 具体来说，包括 function关键字，形参列表，大括号，以及函数体中的内容。
+  return typeof Ctor === "function"
+    && Object.prototype.hasOwnProperty.toString.call(Ctor) === Object.prototype.hasOwnProperty.toString.call(Object)
+}
+
+export const isEmptyObject = function(obj) {
+  for (let name in obj) {
+    return false;
+  }
+  return true
+}
+
+export const isWindow = function(obj) {
+  return obj != null && obj === obj.window
+}
+
+export const isArrayLike = function(obj) {
+  // obj 必须有 length属性
+  let length = !!obj && "length" in obj && obj.length;
+  let typeRes = type(obj);
+
+  // 排除掉函数和 Window 对象
+  if (typeRes === "function" || isWindow(obj)) {
+    return false;
+  }
+
+  return typeRes === "array" || length === 0 ||
+    typeof length === "number" && length > 0 && (length - 1) in obj;
+}
+
+// 判断是否为DOM元素
+export const isElement = function(obj) {
+  return !!(obj && obj.nodeType === 1)
+}
